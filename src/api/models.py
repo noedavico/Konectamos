45c585a2fe38c9e342296d66814ca9511dcecd94
danielbon84 <daniel_bon84@hotmail.com>
2023-03-11 18:03:24 +0000

    hola tu

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,85 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(120), unique=False, nullable=False)
+    direccion = db.Column(db.String(120), unique=False, nullable=False)
+    ciudad = db.Column(db.String(120), unique=False, nullable=False)
+    codigo_postal = db.Column(db.Integer, unique=False, nullable=False)
+    telefono = db.Column(db.String(120), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    es_profesional = db.Column(db.Boolean(), unique=False, nullable=False)
+    #user_profesional = db.relationship( 'professional' , backref = 'user' , lazy = True )
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    #Professional = db.relationship( 'professional' , backref = 'user' , lazy = True )
     def __repr__(self):
         return f'<User {self.email}>'
-
+    
     def serialize(self):
-        return {
+        result = {
             "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "direccion": self.direccion,
+            "ciudad": self.ciudad,
+            "codigo_postal": self.codigo_postal,
+            "telefono": self.telefono,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            
+            
+            }
+        if self.es_profesional:
+            result["profesional"]= "profesional"
+            
+        return result
+    
+class Professional(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(120), unique=False, nullable=False)
+    experiencia = db.Column(db.String(120), unique=False, nullable=False)
+    precio_hora = db.Column(db.String(120), unique=False, nullable=False)
+    puntuacion = db.Column(db.String(120), unique=False, nullable=False)
+    cantidad_votos = db.Column(db.Integer, unique=False, nullable=False)
+    foto_perfil = db.Column(db.String(120), unique=False, nullable=False)
+    redes_sociales = db.Column(db.String(120), unique=False, nullable=False)
+    #id_usuarios = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    #categoria_id = db.Column(db.Integer, unique=False, nullable=False)
+    #user = db . relationship ( 'User' , backref = 'Professional' , lazy = True )
+    
+    def __repr__(self):
+        return f'<Professional {self.id}>'
+    
+    def serialize(self):
+        return  {
+            "id": self.id,
+            "descripcion": self.descripcion,
+            "experiencia": self.experiencia,
+            "precio_hora": self.precio_hora,
+            "puntuacion": self.puntuacion,
+            "cantidad_votos": self.cantidad_votos,
+            "foto_perfil": self.foto_perfil,
+            "redes_sociales": self.redes_sociales,
+            "categoria_id": self.categoria_id,
         }
+
+class Categorias(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    descripcion = db.Column(db.String(120), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f'<Categorias {self.nombre}>'
+    
+    def serialize(self):
+        return  {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Professional, Categorias
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -14,5 +14,61 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
+
+    return jsonify(response_body), 200
+
+
+@api.route('/user', methods=['POST', 'GET'])
+def create_user():
+    request_body = request.json
+    print(request_body)
+    
+
+    user_query = User.query.filter_by(email=request_body["email"]).first()
+
+    if user_query is None:
+        user = User(email=request_body["email"],
+                    password=request_body["password"],
+                    nombre=request_body["nombre"],
+                    apellido=request_body["apellido"],
+                    direccion=request_body["direccion"],
+                    ciudad=request_body["ciudad"],
+                    codigo_postal=request_body["codigo_postal"])
+        # db.session.add(user)
+        # db.session.commit()
+        print("el usuario se a creado",user)
+        
+    else: 
+        print("ya existe el usuario")
+
+    response_body = {
+            "msg": "el usuario se a creado con exito",
+            #"result": user_query.serialize()
+        }
+
+    return jsonify(response_body), 200
+
+
+
+#añadir datos en caso de ser profecional
+def create_professional(request_body):
+    print(request_body)
+    
+
+    user_query = User.query.filter_by(email=request_body["email"]).first()
+
+    if user_query is None:
+        user = User(email=request_body["email"], password=request_body["password"], nombre=request_body["nombre"], apellido=request_body["apellido"], direccion=request_body["direccion"], ciudad=request_body["ciudad"], codigo_postal=request_body["codigo_postal"])
+        # db.session.add(user)
+        # db.session.commit()
+        print("el usuario se a creado",user)
+        
+    else: 
+        print("ya existe el usuario")
+
+    response_body = {
+            "msg": "el usuario se a creado con exito",
+            #"result": user_query.serialize()
+        }
 
     return jsonify(response_body), 200
