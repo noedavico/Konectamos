3195dd38b775f1d57d95bee2bb6e900d02b97b3c
danielbon84 <daniel_bon84@hotmail.com>
2023-03-15 18:09:56 +0000

    back-login

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from .models import db, User, Professional, Categorias
+from .models import db, User
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,8 +11,7 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Professional, db.session))
-    admin.add_view(ModelView(Categorias, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Professional, Categorias
+from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -16,8 +16,7 @@ api = Blueprint('api', __name__)
 @api.route('/user', methods=['POST'])
 def create_user():
     request_body = request.json
-    print(request_body)
-
+    
     user_query = User.query.filter_by(email=request_body["email"]).first()
 
     if user_query is None:
@@ -27,11 +26,7 @@ def create_user():
                     apellido=request_body["apellido"],
                     
                     )
-        # if request_body["es_profesional"]:
-        #     professional = create_professional(request_body)
-        #     professional.professional_user = user
-        #     user.professional_user = professional
-        #     db.session.add(professional)
+        
             
         db.session.add(user)
         db.session.commit()
@@ -48,29 +43,7 @@ def create_user():
     return jsonify(response_body), 200
 
 
-# añadir datos en caso de ser profecional
-def create_professional(request_body):
-    print(request_body)
 
-    return Professional(descripcion=request_body["descripcion"],
-                        experiencia=request_body["experiencia"],
-                        precio_hora=request_body["precio_hora"],
-                        puntuacion=request_body["puntuacion"],
-                        cantidad_votos=request_body["cantidad_votos"],
-                        
-                        redes_sociales=request_body["redes_sociales"],
-                        categoria_id=Categorias.query.filter_by(id=request_body["categoria_id"]).first())
-    # db.session.add(user)
-    # db.session.commit()
-    print("el professional se a creado", professional)
-
-    # else:
-    #     print("ya existe el professional")
-
-    response_body = {
-        "msg": "el professional se a creado con exito",
-        # "result": user_query.serialize()
-    }
 
     return jsonify(response_body), 200
 
@@ -89,17 +62,7 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# obtiene los datos de un usuario
-# @api.route('/user/<int:user_id>', methods=['GET'])
-# def get_info_user(user_id):
-#     user_query = User.query.filter_by(id=user_id).first()
-# ##querys o consultas
-#     response_body = {
-#         "msg": "ok",
-#         "result": user_query.serialize()
-#     }
 
-#     return jsonify(response_body), 200
 
 # Cree una ruta para autenticar a sus usuarios y devolver JWT. El
 # La función create_access_token() se usa para generar realmente el JWT..
