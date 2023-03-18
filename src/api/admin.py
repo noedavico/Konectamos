22084dc5816f16cb9b7372f170125a2d9cc0f1ee
danielbon84 <daniel_bon84@hotmail.com>
2023-03-18 13:51:42 +0000

    con errores

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
-  
+
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, User_info, Foto, Direccion, Categorias, Peques, Mayores, Mascota, Resenas, Valoracion
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,16 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(User_info, db.session))
+    admin.add_view(ModelView(Foto, db.session))
+    admin.add_view(ModelView(Direccion, db.session))
+    admin.add_view(ModelView(Categorias, db.session))
+    admin.add_view(ModelView(Peques, db.session))
+    admin.add_view(ModelView(Mayores, db.session))
+    admin.add_view(ModelView(Mascota, db.session))
+    admin.add_view(ModelView(Resenas, db.session))
+    admin.add_view(ModelView(Valoracion, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, User_info, Foto, Direccion, Categorias, Peques, Mayores, Mascota, Resenas, Valoracion
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
